1e2ca8ca9cd278f0f4dba540b1a544ddfb404f70
Makavura <makavuramughanga@gmail.com>
2021-02-19 10:40:27 +0300

    configure cors for socket io on route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const cors = require('cors');
+const CORS_OPTIONS = {
+    origin: 'http://localhost:3000',
+}
 
-router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+router.get("/",cors(CORS_OPTIONS), (req, res) => {
+    res.send({ response: "I am alive" }).status(200);
 });
 
 module.exports = router;
