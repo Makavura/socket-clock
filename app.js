5149af447d228ab313362a9f65cd4c21a77f06a3
Makavura <makavuramughanga@gmail.com>
2021-02-19 10:15:57 +0300

    setup socket event listeners

--- app.js
+++ app.js
@@ -7,9 +7,26 @@ const index = require("./routes/index");
 
 const app = express();
 app.use(index);
-
 const server = http.createServer(app);
+const io = socketIo(server);
 
-const io = socketIo(server); // < Interesting!
+let interval;
 
-const getApiAndEmit = "TODO";
+io.on("connection", (socket) => {
+    console.log("New client connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        clearInterval(interval);
+    });
+});
+
+const getApiAndEmit = socket => {
+    const response = new Date();
+    socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
